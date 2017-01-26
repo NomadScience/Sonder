4bbc19e7ead9ee344a94884ada977be90d994b54
George A. Michel <sarumonakuba@gmail.com>
2017-01-25 16:49:05 -0800

    (perf) Add R-trees to adjacency indexer; massive speed improvement

--- App/Fixtures/geoHelpers.js
+++ App/Fixtures/geoHelpers.js
@@ -1,5 +1,6 @@
 const turf = require('@turf/turf');
 const vis = require('code42day-vis-why');
+const rtree = require('rtree');
 
 const { clone } = require('cloneextend');
 const Offset = require('polygon-offset');
@@ -91,11 +92,52 @@ exports.findAdjacentHoods = (currentHood, hoodFeatures) => {
   return adjacentHoods;
 }
 
-exports.makeIndexedCollection = (hoodCollection) => {
+const growBBox = (bbox, d = 0.0000001) => [bbox[0]-d,bbox[1]-d,bbox[2]+d,bbox[3]+d];
+
+exports.makeIndexedCollectionFast = (hoodCollection, pure) => {
   const start = Date.now();
-  hoodCollection = clone(hoodCollection);
+  if (pure) { hoodCollection = clone(hoodCollection); }
   const hoods = hoodCollection.features;
-  hoodTotal = hoodCollection.features.length;
+  const hoodTotal = hoods.length;
+  console.log('Initializing adjacent neighborhoods properties');
+  const tree = rtree(10);
+  tree.geoJSON(hoodCollection);
+
+  hoods.forEach((hood, index) => { 
+    hood.properties.index = index;
+    hood.properties.adjacents = [];
+  });
+  let counter = 0;
+  for (let centerIndex = 0; centerIndex < hoodTotal; centerIndex++) {
+    const centerHood = hoods[centerIndex];
+    let centerBBox = growBBox( turf.bbox(centerHood) ); // avoid false negs
+    centerBBox= [centerBBox.slice(0,2), centerBBox.slice(2)];
+    let candidates = tree.bbox(centerBBox);
+    // candidates = Array.from(new Set(candidates).values()); // kill duplicates
+    for (let candidateIndex = 0; candidateIndex < candidates.length; candidateIndex++) {
+      const candidateHood = candidates[candidateIndex];
+      const adjacentIndex = candidateHood.properties.index;
+      if (adjacentIndex === centerIndex) continue;
+      if (centerHood.properties.adjacents.indexOf(adjacentIndex) !== -1) continue;
+      if (turf.intersect(centerHood, candidateHood)) {
+        counter++;
+        centerHood.properties.adjacents.push(adjacentIndex);
+        candidateHood.properties.adjacents.push(centerIndex);
+      }
+    }
+    console.log("Finished "+centerIndex+' of '+hoodTotal+': "' + centerHood.properties.label+'"');
+    console.log("Adjacent indices: " + centerHood.properties.adjacents);
+  }
+  console.log('Elapsed: '+(Date.now()-start)/1000+' seconds');
+  console.log('Intersect called '+counter+' times.');
+  return hoodCollection;
+}
+
+exports.makeIndexedCollection = (hoodCollection, pure) => {
+  const start = Date.now();
+  if (pure) { hoodCollection = clone(hoodCollection); }
+  const hoods = hoodCollection.features;
+  const hoodTotal = hoodCollection.features.length;
   console.log('Initializing adjacent neighborhoods properties');
   // Initialize adjacent properties, add index to make it a little less brittle
   hoods.forEach((hood, index) => { 
@@ -103,9 +145,9 @@ exports.makeIndexedCollection = (hoodCollection) => {
     hood.properties.adjacents = [];
   });
   let counter = 0;
-  for (var centerIndex = 0; centerIndex < hoodTotal; centerIndex++) {
+  for (let centerIndex = 0; centerIndex < hoodTotal; centerIndex++) {
     const centerHood = hoods[centerIndex];
-    for (var adjacentIndex = centerIndex+1; adjacentIndex < hoodTotal; adjacentIndex++) {
+    for (let adjacentIndex = centerIndex+1; adjacentIndex < hoodTotal; adjacentIndex++) {
       const adjacentHood = hoods[adjacentIndex];
       if (centerHood.properties.adjacents.indexOf(adjacentIndex) !== -1) continue;
       if (turf.intersect(centerHood, adjacentHood)) {
